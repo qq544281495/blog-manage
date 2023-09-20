7a5c6fccaee0a268f816a86880b0246e30fa7796
unknown <544281495@qq.com>
2023-09-20 10:48:35 +0800

    docs: 修改eslint规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   rules: {
-    'space-before-function-paren': 'off' // 关闭方法名与括号之间必须有一个空格规范
+    'space-before-function-paren': 'off', // 关闭方法名与括号之间必须有一个空格规范
+    'vue/comment-directive': 'off'
   }
 }
